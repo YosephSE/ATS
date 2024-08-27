f0a3a5e83979433c8be918eac515bb74eb5cd24e
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-08-27 16:18:11 +0300

    hot fix

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
-import express, { Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/DB";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 dotenv.config();
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 3000;
+app.use(cookieParser());
+app.use(cors());
+connectDB();
 
-app.get('/', (req: Request, res: Response) => {
-  res.json({message: 'Hello, TypeScript with Express!'});
-});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
