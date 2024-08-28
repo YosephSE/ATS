477fab55c0a582bb9c963a5faa653cf46d20eeb9
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-08-28 13:49:20 +0300

    admins schema defined

---
+++ server/src/models/admins.ts
@@ -0,0 +1,35 @@
+import { Schema, model } from "mongoose";
+
+const adminSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
