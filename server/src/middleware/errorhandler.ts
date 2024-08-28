51e3daa1783c309c739fb03491727e927fda3aa6
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-08-29 00:14:22 +0300

    minor

--- server/src/middleware/errorhandler.ts
+++ server/src/middleware/errorhandler.ts
@@ -1,11 +1,10 @@
 import {Request, Response, NextFunction} from 'express';
 
 const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-    console.error(err.stack);  // Log the error stack for debugging purposes
+    console.error(err.stack);  
 
-    // Handle specific types of errors
+
     if (err.name === 'ValidationError') {
-        // Handle Mongoose validation error
         return res.status(400).json({
             message: 'Validation Error',
             errors: err.errors,
@@ -13,27 +12,24 @@ const errorHandler = (err: any, req: Request, res: Response, next: NextFunction)
     }
 
     if (err.name === 'CastError' && err.kind === 'ObjectId') {
-        // Handle Mongoose CastError (invalid ObjectId)
         return res.status(400).json({
             message: 'Invalid ID format',
         });
     }
 
     if (err.status === 404) {
-        // Handle not found errors
+
         return res.status(404).json({
             message: 'Resource not found',
         });
     }
 
     if (err.name === 'UnauthorizedError') {
-        // Handle JWT authentication errors
         return res.status(401).json({
             message: 'Unauthorized access',
         });
     }
 
-    // Default to 500 Internal Server Error
     res.status(500).json({
         message: 'Internal Server Error',
         error: err.message,
