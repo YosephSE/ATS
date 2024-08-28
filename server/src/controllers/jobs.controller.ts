11de41bc8f3ccce5d0013c5918f0b6ef4937645c
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-08-28 16:30:52 +0300

    Job basic CRUD applied"

--- server/src/controllers/jobs.controller.ts
+++ server/src/controllers/jobs.controller.ts
@@ -5,7 +5,7 @@ import asyncHandler from "express-async-handler";
 const allJobs = asyncHandler(async (req: Request, res: Response) => {
   try {
     const jobs = await Job.find();
-    res.json(jobs);
+    res.status(200).json(jobs);
   } catch (error) {
     res.status(500).json({ message: (error as Error).message });
   }
@@ -14,7 +14,7 @@ const allJobs = asyncHandler(async (req: Request, res: Response) => {
 const singleJob = async (req: Request, res: Response) => {
   try {
     const job = await Job.findById(req.params.id);
-    res.json(job);
+    res.status(200).json(job);
   } catch (error) {
     res.status(500).json({ message: (error as Error).message });
   }
@@ -24,11 +24,30 @@ const createJob = async (req: Request, res: Response) => {
   try {
     const newJob = new Job(req.body);
     await newJob.save();
-    res.json(newJob);
+    res.status(201).json({ message: "Job created successfully" });
   } catch (error) {
     res.status(500).json({ message: (error as Error).message });
   }
 };
 
+const updateJob = asyncHandler(async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const updatedJob = await Job.findByIdAndUpdate(id, req.body, { new: true });
+    res.status(201).json({ message: "Job updated successfully" });
+  } catch (error) {
+    res.status(500).json({ message: (error as Error).message });
+  }
+});
 
-export { allJobs, singleJob, createJob };
+const deleteJob = asyncHandler(async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    await Job.findByIdAndDelete(id);
+    res.status(201).json({ message: "Job deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: (error as Error).message });
+  }
+});
+
+export { allJobs, singleJob, createJob, updateJob, deleteJob };
--- server/src/routes/jobs.route.ts
+++ server/src/routes/jobs.route.ts
@@ -1,10 +1,22 @@
 import { Router } from "express";
-import { allJobs, createJob } from "../controllers/jobs.controller";
+import {
+  allJobs,
+  createJob,
+  singleJob,
+  updateJob,
+  deleteJob,
+} from "../controllers/jobs.controller";
 
 const router = Router();
 
 router.get("/", allJobs);
 
 router.post("/", createJob);
+
+router.get("/:id", singleJob);
+
+router.put("/:id", updateJob);
+
+router.delete("/:id", deleteJob);
 
 export default router;
