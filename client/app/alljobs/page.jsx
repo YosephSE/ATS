a21cf1b0f16e331132f000fd87da2ede455e0373
eyoelm5 <eyoelmoges20@gmail.com>
2024-09-05 23:33:17 +0300

    Adding all jobs page for admins

---
+++ client/app/alljobs/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Sidebar from '@/components/SideBar';
+
+const allJobs = () => {
+  return (
+    <div className='flex bg-[#F8FDFF]'>
+        <Sidebar />
+        <div className="w-[80%] mx-auto p-6 min-h-screen">
+          <h2 className="text-3xl font-bold text-blue-600 mb-6">All Jobs</h2>
+          <div>
+
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default allJobs;
