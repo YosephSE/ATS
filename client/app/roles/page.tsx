98a92ee3a371c22e24da90a4851bd11e1d5d62aa
eyoelm5 <eyoelmoges20@gmail.com>
2024-09-05 12:20:47 +0300

    Add a Header component and roles page

--- client/app/roles/page.tsx
+++ client/app/roles/page.tsx
@@ -1,13 +1,78 @@
+"use client"
 import React from 'react'
+import { Button } from '@mui/material'
+import Header from '@/components/Header'
+import { useAppDispatch } from '@/redux/Hooks'
+import Modal from "../../components/Modal"
+import { setLogin, setRegister } from '@/redux/slices/ModalSlice'
 
 const RolesPage = () => {
-  return (
-    <div className='grid grid-cols-2 min-h-screen'>
-        <div className='bg-[radial-gradient(circle,#94CFFA,#F5FFF6)]'>
+    const dispatch = useAppDispatch()
+    return (
+        <div className='relative min-h-screen flex flex-col'>
+            <div className='sticky top-0 z-10'>
+                <Header page="roles" />
+            </div>
+            <div className='grid md:grid-cols-2 min-h-scren'>
+                <div className='bg-[radial-gradient(circle,#94CFFA,#F5FFF6)] flex justify-center items-center min-h-screen'>
+                    <div className='w-[80%] flex flex-col items-center gap-10'>
+                        <h1 className='text-3xl text-center'>For Employee</h1>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt, doloremque beatae! Culpa dignissimos animi nulla, eligendi minima in ea?</p>
+                        <Button
+                        variant='contained'
+                        sx={{
+                            backgroundColor: '#F8FDFF',
+                            color: '#000',
+                            width: '120px',
+                            height: '40px',
+                            borderRadius: '10px',
+                            '&:hover': {
+                                backgroundColor: '#1565c0',
+                                color: 'white'
+                            },
+                            '&.Mui-disabled': {
+                                backgroundColor: '#1976d2', 
+                                color: 'white', 
+                            },
+                        }}
+                        onClick = {() => dispatch(setLogin())} 
+                        >
+                            Login
+                        </Button>
+                        <p className='text-center mt-5'>Don’t have an account? <span className='text-blue-500 hover:underline hover:text-blue-700 hover:cursor-pointer' onClick={() => dispatch(setRegister())}>Contact Admin</span></p>
+                    </div>
+                </div>
+                <div className='flex justify-center items-center min-h-screen'>
+                    <div className='w-[80%] flex flex-col items-center gap-10'>
+                        <h1 className='text-3xl text-center'>For Candidate</h1>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt, doloremque beatae! Culpa dignissimos animi nulla, eligendi minima in ea?</p>
+                        <Button
+                        variant='contained'
+                        sx={{
+                            width: '120px',
+                            height: '40px',
+                            borderRadius: '10px',
+                            '&:hover': {
+                                backgroundColor: '#1565c0',
+                                color: 'white'
+                            },
+                            '&.Mui-disabled': {
+                                backgroundColor: '#1976d2', 
+                                color: 'white', 
+                            },
+                        }}
+                        onClick = {() => dispatch(setLogin())} 
+                        >
+                            Login
+                        </Button>
+                        <p className='text-center mt-5'>Don’t have an account? <span className='text-blue-500 hover:underline hover:text-blue-700 hover:cursor-pointer' onClick={() => dispatch(setRegister())}>Sign Up</span></p>
+                    </div>
+                </div>
+            </div>
+
+            <Modal />
         </div>
-        <div></div>
-    </div>
-  )
+    )
 }
 
 export default RolesPage
--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 import { Button } from '@mui/material'
 import { useAppDispatch } from '@/redux/Hooks'
@@ -11,10 +11,21 @@ interface Props {
 
 const Header = ({ page }: Props) => {
     const dispatch = useAppDispatch()
+    const [roles, setRoles] = useState(false)
+    const [home, setHome] = useState(false)
+
+    useEffect(() => {
+        if(page === "home"){
+            setHome(true)
+        } else if(page === "roles") {
+            setRoles(true)
+        }
+    }, [])
+
     return (
-        <nav className="flex items-center justify-between px-6 py-3 border-b shadow-lg">
+        <nav className={`flex items-center justify-between px-6 py-3 ${home && "border-b shadow-lg"} ${roles && "absolute w-full top-0"}`}>
             <Link href="/" className="flex items-center space-x-2 focus:outline-none">
-            <span className="text-xl md:text-2xl font-medium text-blue-700 hover:text-blue-800">Company Name</span>
+            <span className="text-xl md:text-2xl font-medium text-blue-700 hover:text-blue-800">Company{roles && <br/>} Name</span>
             </Link>
             <div className="flex items-center space-x-6">
                 <Link href="/jobs" className="text-base text-gray-600 hover:text-gray-800">
