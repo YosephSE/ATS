b27bb3639a83776ecfd25c44e705fa079f2e5c57
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-09-06 03:00:46 +0300

    stats folder in components

--- client/app/alljobs/page.jsx
+++ client/app/alljobs/page.jsx
@@ -8,6 +8,7 @@ const allJobs = () => {
         <div className="w-[80%] mx-auto p-6 min-h-screen">
           <h2 className="text-3xl font-bold text-blue-600 mb-6">All Jobs</h2>
           <div>
+            
 
           </div>
         </div>
