29e039fc8024c2b8bd45bc0bb3dcbdedb86b3735
eyoelm5 <eyoelmoges20@gmail.com>
2024-09-06 15:30:56 +0300

    Adjust Modal slice to handle users

--- client/redux/slices/ModalSlice.tsx
+++ client/redux/slices/ModalSlice.tsx
@@ -2,50 +2,41 @@ import { createSlice } from "@reduxjs/toolkit"
 
 interface ModalState{
     value: "closed" | "signin" | "signup" | "contact"
+    user: "admin" | "candidate"
 }
 
 const initialState: ModalState = {
-    value: "closed"
+    value: "closed",
+    user: "candidate"
 }
 
 const ModalSlice = createSlice({
     name: "modal",
     initialState,
     reducers: {
-        setLogin: (state) => {
-            state = {
-                value: "signin"
-            }
-
-            return state
+        setLoginAdmin: (state) => {
+            state.value = "signin"
+            state.user = "admin"
+        },
+        setLoginCandidate: (state) => {
+            state.value = "signin"
+            state.user = "candidate"
         },
         setRegister: (state) =>{
-            state = {
-                value: "signup"
-            }
-
-            return state
+            state.value = "signup"
         },
         setContact: (state) => {
-            state = {
-                value: "contact"
-            }
-
-            return state
+            state.value = "contact"
         }, 
 
         setClosed: (state) => {
-            state = {
-                value: "closed"
-            }
-
-            return state
+            state.value = "closed"
         }
 
 
     }
 })
 
-export const { setClosed, setRegister, setLogin, setContact} = ModalSlice.actions
+export const { setClosed, setRegister, setLoginAdmin, setLoginCandidate, setContact} = ModalSlice.actions
 
 export default ModalSlice.reducer;
