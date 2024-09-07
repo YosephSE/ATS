1469dac5a5a0aa0a4799c6931066c7bd860863df
eyoelm5 <eyoelmoges20@gmail.com>
2024-09-08 00:54:12 +0300

    Proxy fix

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         return [
           {
             source: '/api/:path*', 
-            destination: `${process.env.NEXT_PUBLIC_BACKEND_SERVER}/api/:path*`,
+            destination: `${process.env.NEXT_PUBLIC_BACKEND_SERVER}/:path*`,
           },
         ];
       },
