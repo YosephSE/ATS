1d2e04aea3ac4d2dde9edaa387d31b6c91270c21
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-09-07 22:14:05 +0300

    candidate profile can upload image

--- client/components/candidate/Profile.tsx
+++ client/components/candidate/Profile.tsx
@@ -1,5 +1,6 @@
 import React, { useState, ChangeEvent } from "react";
 import { Edit2, X } from "lucide-react";
+import uploadImage from "@/utils/imageUploader";
 
 interface ProfileData {
   firstName: string;
@@ -7,88 +8,19 @@ interface ProfileData {
   password: string;
   phoneNumber: string;
   email: string;
-  skills: string[];
-  education: {
-    schoolName: string;
-    degree: string;
-    fieldOfStudy: string;
-    startYear: number;
-    endYear: number;
-  }[];
-  experience: {
-    title: string;
-    company: string;
-    location: string;
-    startDate: string;
-    endDate: string;
-    description: string;
-  }[];
 }
 
-const user = {
-  _id: "66d8ca6d3af2918c82e14976",
-  firstName: "Y",
-  lastName: "K",
-  email: "y.k@g.com",
-  skills: ["JavaScript", "Node.js", "MongoDB", "React", "Python"],
-  bookmarks: ["64b7c92e5d8e4b001a1b1c5a", "64b7c97e5d8e4b001a1b1c5b"],
-  education: [
-    {
-      _id: "66dc571db519611384150f6e",
-      schoolName: "Harvard University",
-      degree: "Bachelor of Science",
-      fieldOfStudy: "Computer Science",
-      startYear: 2015,
-      endYear: 2019,
-    },
-    {
-      _id: "66dc571db519611384150f6f",
-      schoolName: "Harvard University",
-      degree: "Bachelor of Science",
-      fieldOfStudy: "Computer Science",
-      startYear: 2015,
-      endYear: 2019,
-    },
-  ],
-  experience: [
-    {
-      _id: "66dc571db519611384150f70",
-      title: "Software Engineer",
-      company: "Tech Corp",
-      location: "New York, NY",
-      startDate: "2019-06-01T00:00:00.000Z",
-      endDate: "2021-08-31T00:00:00.000Z",
-      description:
-        "Developed and maintained web applications using JavaScript, Node.js, and MongoDB.",
-    },
-    {
-      _id: "66dc571db519611384150f71",
-      title: "Senior Software Engineer",
-      company: "Innovatech",
-      location: "San Francisco, CA",
-      startDate: "2021-09-01T00:00:00.000Z",
-      endDate: "2023-03-01T00:00:00.000Z",
-      description:
-        "Led a team of developers in building scalable web solutions and microservices architecture.",
-    },
-  ],
-  __v: 0,
-  linkedIn: "https://linkedin.com/in/johndoe",
-  resume: "https://example.com/resume/johndoe.pdf",
-};
 const AdminProfile: React.FC = () => {
   const [isEditing, setIsEditing] = useState<boolean>(false);
   const [profileData, setProfileData] = useState<ProfileData>({
-    firstName: user.firstName,
-    lastName: user.lastName,
+    firstName: "Yoseph",
+    lastName: "Kedir",
     password: "",
     phoneNumber: "0912345678",
-    email: user.email,
-    skills: user.skills,
-    education: user.education,
-    experience: user.experience,
+    email: "yoseph.kedir10@gmail.com",
   });
-  const [image, setImage] = useState<string | null>(null);
+  const [image, setImage] = useState<File | null>(null);
+  const [imgLink, setImgLink] = useState<string | null>(null);
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -97,12 +29,15 @@ const AdminProfile: React.FC = () => {
 
   const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files[0]) {
-      setImage(URL.createObjectURL(e.target.files[0]));
+      setImage(e.target.files[0] as File);
     }
   };
 
-  const handleUpdateProfile = () => {
-    // Here you would typically send the updated data to your backend
+  const handleUpdateProfile = async () => {
+    if (image) {
+      const uploadedImgLink = await uploadImage(image) || null;
+      setImgLink(uploadedImgLink);
+    }
     setIsEditing(false);
   };
 
@@ -110,9 +45,9 @@ const AdminProfile: React.FC = () => {
     <div className="max-w-md mx-auto mt-10 bg-white rounded-lg shadow-md overflow-hidden">
       <div className="p-6">
         <div className="flex items-center justify-center mb-4">
-          {image ? (
+          {imgLink ? (
             <img
-              src={image}
+              src={imgLink}
               alt={profileData.firstName}
               className="w-24 h-24 rounded-full object-cover"
             />
@@ -132,7 +67,19 @@ const AdminProfile: React.FC = () => {
           Phone Number: {profileData.phoneNumber}
         </p>
 
-        {isEditing ? (
+        {!isEditing ? (
+          <div className="flex justify-center space-x-2">
+            <button
+              onClick={() => setIsEditing(true)}
+              className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300"
+            >
+              Edit Profile
+            </button>
+            <button className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition duration-300">
+              Delete
+            </button>
+          </div>
+        ) : (
           <div className="space-y-4">
             <input
               type="text"
@@ -194,60 +141,6 @@ const AdminProfile: React.FC = () => {
                 Cancel
               </button>
             </div>
-          </div>
-        ) : (
-          <div>
-            <div className="flex justify-center space-x-2">
-              <button
-                onClick={() => setIsEditing(true)}
-                className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300"
-              >
-                Edit Profile
-              </button>
-              <button className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition duration-300">
-                Delete
-              </button>
-            </div>
-            <h3 className="text-lg font-semibold mt-4">Skills</h3>
-            <p>{profileData.skills.join(", ")}</p>
-            <h3 className="text-lg font-semibold mt-4">Education</h3>
-            <ul>
-              {profileData.education.map((edu, index) => (
-                <li key={index} className="mb-2">
-                  {edu.degree} in {edu.fieldOfStudy} from {edu.schoolName} (
-                  {edu.startYear} - {edu.endYear})
-                </li>
-              ))}
-            </ul>
-            <h3 className="text-lg font-semibold mt-4">Experience</h3>
-            <ul>
-              {profileData.experience.map((exp, index) => (
-                <li key={index} className="mb-2">
-                  <strong>{exp.title}</strong> at {exp.company}, {exp.location}{" "}
-                  ({new Date(exp.startDate).toLocaleDateString()} -{" "}
-                  {new Date(exp.endDate).toLocaleDateString()})
-                  <p>{exp.description}</p>
-                </li>
-              ))}
-            </ul>
-            <h3 className="text-lg font-semibold mt-4">LinkedIn</h3>
-            <a
-              href={user.linkedIn}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-600"
-            >
-              {user.linkedIn}
-            </a>
-            <h3 className="text-lg font-semibold mt-4">Resume</h3>
-            <a
-              href={user.resume}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-600"
-            >
-              {user.resume}
-            </a>
           </div>
         )}
       </div>
