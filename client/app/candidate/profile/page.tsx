7e309be0f521621c62418f14d6dd2c267c34d50e
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-09-09 00:48:32 +0300

    Header to profile page

--- client/app/candidate/profile/page.tsx
+++ client/app/candidate/profile/page.tsx
@@ -1,11 +1,13 @@
 "use client"
 import React from 'react'
 import CandidteProfile from '@/components/candidate/Profile';
+import Header from '@/components/Header';
 
 const AllApp = () => {
   return (
+    <>
+     <Header page='home' />
     <div className='flex bg-[#F8FDFF]'>
-    
         <div className="w-full mx-auto p-6 min-h-screen">
           <h2 className="text-3xl font-bold text-blue-600 mb-6 pl-80">Profile</h2>
           <div>
@@ -13,6 +15,7 @@ const AllApp = () => {
           </div>
         </div>
     </div>
+    </>
   )
 }
 
