17cec94fe132bd36a5de2af4f1e89e25e6bb10d7
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-09-09 03:21:48 +0300

    Store variabe updated

--- client/app/StoreProvider.tsx
+++ client/app/StoreProvider.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useRef } from 'react'
 import { Provider } from 'react-redux'
-import { makeStore, AppStore } from '../redux/store'
+import { makeStore, AppStore } from '../redux/Store'
 
 
 export default function StoreProvider({
