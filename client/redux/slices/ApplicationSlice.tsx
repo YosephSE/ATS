e40623c5980d39657b45fff7468fe689b41ce625
eyoelm5 <eyoelmoges20@gmail.com>
2024-09-09 07:16:16 +0300

    Send correct data to backend

--- client/redux/slices/ApplicationSlice.tsx
+++ client/redux/slices/ApplicationSlice.tsx
@@ -37,7 +37,7 @@ export const apply = createAsyncThunk(
   "applications/apply",
   async(id: string, { rejectWithValue}) => {
     try {
-      const response = await axios.post(`${api}/applications`, id)
+      const response = await axios.post(`${api}/applications`, {jobId: id})
       return response.data
     } catch (error: any) {
       return rejectWithValue(error.response?.data?.error || error.response?.data?.message);
