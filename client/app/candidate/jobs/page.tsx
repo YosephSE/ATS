8810a69addfe3a7d92a097bbe44191678b1c6a73
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-09-10 20:34:26 +0300

    loadin state added and integrated

--- client/app/candidate/jobs/page.tsx
+++ client/app/candidate/jobs/page.tsx
@@ -9,59 +9,63 @@ import { useAppDispatch, useAppSelector } from "@/redux/Hooks";
 import { RootState } from "@/redux/Store";
 import { acitvejobs } from "@/redux/slices/JobSlice";
 import SingleJob from "@/components/SingleJob";
+import JobListingLoadingSkeleton from "@/components/skeletons/candidateJobs";
 
 const Jobs = () => {
   const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    const fetchJobs = async () => {
-      await dispatch(acitvejobs(""));
-    };
-
-    fetchJobs();
-  }, []);
-
+  const [loading, setLoading] = useState(true); 
   const [open, setOpen] = useState(false);
   const currentState = useAppSelector((state: RootState) => state.jobs);
   const alljobs = currentState.activeJobs;
 
+  useEffect(() => {
+    const fetchJobs = async () => {
+      setLoading(true); 
+      await dispatch(acitvejobs(""));
+      setLoading(false); 
+    };
+
+    fetchJobs();
+  }, [dispatch]);
+
+  if (loading) {
+    return <JobListingLoadingSkeleton />; 
+  }
+
   return (
     <div className="min-h-screen bg-gradient-to-b from-blue-50 to-gray-100">
       <Header page="home" />
-          <main className="container mx-auto mt-8 px-4 lg:px-10">
-          <div className="flex justify-center">
-            <Tabs value={0} className="mb-6 border-b-2 border-gray-300">
-              <Tab
-                label="Jobs"
-                className="text-lg font-semibold text-blue-600 hover:text-blue-700 transition-colors"
-              />
-            </Tabs>
-          </div>
-  
-          <div className="flex flex-col lg:flex-row gap-8">
-            <div className="lg:w-2/5">
-              <div className="flex items-center mb-6">
-                <IconButton onClick={() => setOpen(!open)}>
-                  <Tune className="text-blue-600" />
-                </IconButton>
-                <h6 className="text-2xl font-semibold ml-3">Filters</h6>
-              </div>
-  
-              {open && <FilterPanel />}
-  
-              <div className="max-h-[500px] overflow-y-auto space-y-4 px-4 py-2">
-                {alljobs?.map((job) => (
-                  <JobCard key={job._id} job={job} />
-                ))}
-              </div>
+      <main className="container mx-auto mt-8 px-4 lg:px-10">
+        <div className="flex justify-center">
+          <Tabs value={0} className="mb-6 border-b-2 border-gray-300">
+            <Tab
+              label="Jobs"
+              className="text-lg font-semibold text-blue-600 hover:text-blue-700 transition-colors"
+            />
+          </Tabs>
+        </div>
+
+        <div className="flex flex-col lg:flex-row gap-8">
+          <div className="lg:w-2/5">
+            <div className="flex items-center mb-6">
+              <IconButton onClick={() => setOpen(!open)}>
+                <Tune className="text-blue-600" />
+              </IconButton>
+              <h6 className="text-2xl font-semibold ml-3">Filters</h6>
             </div>
-            {
-             alljobs?.length 
-             ?
-             <SingleJob />
-             :
-             (        
-             <div className="flex items-center justify-center bg-gradient-to-b from-blue-50 to-gray-100">
+
+            {open && <FilterPanel />}
+
+            <div className="max-h-[500px] overflow-y-auto space-y-4 px-4 py-2">
+              {alljobs?.map((job) => (
+                <JobCard key={job._id} job={job} />
+              ))}
+            </div>
+          </div>
+          {alljobs?.length ? (
+            <SingleJob />
+          ) : (
+            <div className="flex items-center justify-center bg-gradient-to-b from-blue-50 to-gray-100">
               <div className="text-center p-8 bg-white shadow-lg rounded-lg">
                 <h1 className="text-4xl font-bold text-gray-800 mb-4">No Jobs</h1>
                 <p className="text-lg text-gray-600 mb-6">
@@ -72,10 +76,9 @@ const Jobs = () => {
                 </button>
               </div>
             </div>
-            )
-            }
-          </div>
-        </main>
+          )}
+        </div>
+      </main>
     </div>
   );
 };
