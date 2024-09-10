679f30f150363dbbd9a77fe7bee08fbc95a2e13c
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-09-10 18:25:32 +0300

    active jobs

--- client/components/stats/Stats.tsx
+++ client/components/stats/Stats.tsx
@@ -51,7 +51,7 @@ const Stats = ({
           />
         </Grid>
         <Grid item xs={12} sm={4}>
-          <StatCard
+          <StatCard 
             icon={<Work className="text-blue-400" />}
             value={totalJobs}
             label="Jobs"
