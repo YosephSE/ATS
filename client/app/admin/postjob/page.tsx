cca609adb0152e976fb7503f006179a0e8c99fc3
eyoelm5 <eyoelmoges20@gmail.com>
2024-09-11 16:34:48 +0300

    Minor ui fix

--- client/app/admin/postjob/page.tsx
+++ client/app/admin/postjob/page.tsx
@@ -3,7 +3,7 @@ import JobForm from '@/components/JobForm';
 
 const PostJob = () => {
   return (
-    <div className='flex bg-[#F8FDFF]'>
+    <div className='flex min-h-screen bg-[#F8FDFF]'>
         <Sidebar />
         <div className="w-[80%] mx-auto p-6">
           <h2 className="text-3xl font-bold text-blue-600 mb-6">Post Job</h2>
