43e6e4daa7753d70d6429249164f66db2fbc4627
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-09-15 21:10:56 +0300

    my applications theme weirdness fixed

--- client/components/candidate/ApplicationList.tsx
+++ client/components/candidate/ApplicationList.tsx
@@ -1,111 +1,59 @@
-"use client"
+"use client";
 import { useAppDispatch, useAppSelector } from "@/redux/Hooks";
 import { myapplications } from "@/redux/slices/UserSlice";
 import { RootState } from "@/redux/Store";
 import React, { useEffect } from "react";
 
 const ApplicationList = () => {
-  const applications = useAppSelector((state: RootState) => state.user.applications)
-  const dispatch = useAppDispatch()
+  const applications = useAppSelector(
+    (state: RootState) => state.user.applications
+  );
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
     const fetchApplications = async () => {
-      await dispatch(myapplications())
-    }
+      await dispatch(myapplications());
+    };
 
-    fetchApplications()
-  }, [dispatch])
+    fetchApplications();
+  }, [dispatch]);
 
   return (
     <div className="max-w-[1000px] mx-auto">
-      <div className="relative flex flex-col w-full h-full dark:text-slate-200 dark:bg-slate-700 bg-white shadow-md rounded-xl bg-clip-border">
+      <div className="relative flex flex-col w-full h-full dark:text-slate-200 dark:bg-slate-800 bg-white shadow-md rounded-xl bg-clip-border">
         <div className="p-6">
           <table className="w-full text-left table-auto min-w-max">
             <thead>
               <tr>
-                <th className="p-4 transition-colors cursor-pointer border-y border-slate-200 bg-slate-50 dark:bg-slate-700 dark:hover:bg-slate-600 dark:border-slate-500 hover:bg-slate-100">
-                  <p className="flex items-center justify-between gap-2 font-sans text-sm font-normal leading-none">
-                    Title
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth="2"
-                      stroke="currentColor"
-                      aria-hidden="true"
-                      className="w-4 h-4"
+                {["Title", "Location", "Status", "Department", ""].map(
+                  (header, index) => (
+                    <th
+                      key={index}
+                      className="p-4 transition-colors cursor-pointer border-y border-slate-200 bg-slate-100 dark:bg-slate-700 dark:hover:bg-slate-600 dark:border-slate-500 hover:bg-slate-200"
                     >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9"
-                      ></path>
-                    </svg>
-                  </p>
-                </th>
-                <th className="p-4 transition-colors cursor-pointer border-y border-slate-200 bg-slate-50 dark:bg-slate-700 dark:hover:bg-slate-600 dark:border-slate-500 hover:bg-slate-100">
-                  <p className="flex items-center justify-between gap-2 font-sans text-sm font-normal leading-none ">
-                    Location
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth="2"
-                      stroke="currentColor"
-                      aria-hidden="true"
-                      className="w-4 h-4"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9"
-                      ></path>
-                    </svg>
-                  </p>
-                </th>
-                <th className="p-4 transition-colors cursor-pointer border-y border-slate-200 bg-slate-50 dark:bg-slate-700 dark:hover:bg-slate-600 dark:border-slate-500 hover:bg-slate-100">
-                  <p className="flex items-center justify-between gap-2 font-sans text-sm font-normal leading-none">
-                    Status
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth="2"
-                      stroke="currentColor"
-                      aria-hidden="true"
-                      className="w-4 h-4"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9"
-                      ></path>
-                    </svg>
-                  </p>
-                </th>
-                <th className="p-4 transition-colors cursor-pointer border-y border-slate-200 bg-slate-50 dark:bg-slate-700 dark:hover:bg-slate-600 dark:border-slate-500 hover:bg-slate-100">
-                  <p className="flex items-center justify-between gap-2 font-sans text-sm font-normal leading-none">
-                    Department
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth="2"
-                      stroke="currentColor"
-                      aria-hidden="true"
-                      className="w-4 h-4"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9"
-                      ></path>
-                    </svg>
-                  </p>
-                </th>
-                <th className="p-4 transition-colors cursor-pointer border-y border-slate-200 bg-slate-50 dark:bg-slate-700 dark:hover:bg-slate-600 dark:border-slate-500 hover:bg-slate-100">
-                  <p className="flex items-center justify-between gap-2 font-sans text-sm font-normal leading-none text-slate-100"></p>
-                </th>
+                      <p className="flex items-center justify-between gap-2 font-sans text-sm font-normal leading-none">
+                        {header}
+                        {header && (
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="2"
+                            stroke="currentColor"
+                            aria-hidden="true"
+                            className="w-4 h-4"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9"
+                            ></path>
+                          </svg>
+                        )}
+                      </p>
+                    </th>
+                  )
+                )}
               </tr>
             </thead>
             <tbody>
@@ -114,10 +62,10 @@ const ApplicationList = () => {
                   <td className="p-4 border-b border-slate-200">
                     <div className="flex items-center gap-3">
                       <div className="flex flex-col">
-                        <p className="text-sm font-semibold text-slate-700">
+                        <p className="text-sm font-semibold text-slate-800 dark:text-slate-200">
                           {application.jobId.title}
                         </p>
-                        <p className="text-sm text-slate-100">
+                        <p className="text-sm text-slate-600 dark:text-slate-400">
                           {application.jobId.type}
                         </p>
                       </div>
@@ -125,30 +73,34 @@ const ApplicationList = () => {
                   </td>
                   <td className="p-4 border-b border-slate-200">
                     <div className="flex flex-col">
-                      <p className="text-sm font-semibold text-slate-700">
+                      <p className="text-sm font-semibold text-slate-800 dark:text-slate-200">
                         {application.jobId.location}
                       </p>
                     </div>
                   </td>
                   <td className="p-4 border-b border-slate-200">
                     <div className="w-max">
-                      <div className={`relative grid items-center px-2 py-1 font-sans text-xs font-bold uppercase rounded-md select-none whitespace-nowrap ${
-                        application.status === 'pending'
-                          ? 'text-yellow-900 bg-yellow-500/20'
-                          : application.status === 'approved'
-                          ? 'text-green-900 bg-green-500/20'
-                          : 'text-red-900 bg-red-500/20'
-                      }`}>
-                        <span className="">{application.status}</span>
+                      <div
+                        className={`relative grid items-center px-2 py-1 font-sans text-xs font-bold uppercase rounded-md select-none whitespace-nowrap ${
+                          application.status === "pending"
+                            ? "text-yellow-900 bg-yellow-500/20 dark:bg-yellow-600/20"
+                            : application.status === "approved"
+                            ? "text-green-900 bg-green-500/20 dark:bg-green-600/20"
+                            : "text-red-900 bg-red-500/20 dark:bg-red-600/20"
+                        }`}
+                      >
+                        <span>{application.status}</span>
                       </div>
                     </div>
                   </td>
                   <td className="p-4 border-b border-slate-200">
-                    <p className="text-sm text-slate-100">{application.jobId.department}</p>
+                    <p className="text-sm text-slate-800 dark:text-slate-200">
+                      {application.jobId.department}
+                    </p>
                   </td>
                   <td className="p-4 border-b border-slate-200">
                     <button
-                      className="relative h-10 max-h-[40px] w-10 max-w-[40px] select-none rounded-lg text-center align-middle font-sans text-xs font-medium uppercase text-slate-900 transition-all hover:bg-slate-900/10 active:bg-slate-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                      className="relative h-10 max-h-[40px] w-10 max-w-[40px] select-none rounded-lg text-center align-middle font-sans text-xs font-medium uppercase text-slate-800 dark:text-slate-200 transition-all hover:bg-slate-900/10 active:bg-slate-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                       type="button"
                     >
                       <span className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
